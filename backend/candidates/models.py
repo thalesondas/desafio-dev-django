6dbb01cad115075559419bda33604d5129ffce50
Thales <thalesondas0@gmail.com>
2024-10-20 10:10:35 -0300

    Adding relationships to the models

--- backend/candidates/models.py
+++ backend/candidates/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class PersonalInfo(models.Model):
     name = models.CharField(max_length=255);
     date_of_birth = models.DateField();
+    contact_info = models.OneToOneField("ContactInfo", on_delete=models.CASCADE , related_name="personal_info");
 
 class ContactInfo(models.Model):
     email = models.EmailField();
@@ -17,9 +18,11 @@ class ProfessionalExperience(models.Model):
     start_date = models.DateField();
     end_date = models.DateField(blank=True, null=True);
     description = models.TextField();
+    personal_info = models.ForeignKey("PersonalInfo", on_delete=models.CASCADE, related_name="professional_experiences");
 
 class AcademicBackground(models.Model):
     institution = models.CharField(max_length=255);
     course = models.CharField(max_length=255);
     start_date = models.DateField();
     end_date = models.DateField(blank=True, null=True);
+    personal_info = models.ForeignKey("PersonalInfo", on_delete=models.CASCADE, related_name="academic_backgrounds")
