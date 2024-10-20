3329dde6a821988820963e9f398a38d9be4beac0
Thales <thalesondas0@gmail.com>
2024-10-20 15:24:58 -0300

    Adding custom __str__ for each model to make admin's life easier

--- backend/candidates/admin.py
+++ backend/candidates/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import PersonalInfo, ContactInfo, ProfessionalExperience, AcademicBackground
 
-admin.site.register(PersonalInfo)
 admin.site.register(ContactInfo)
+admin.site.register(PersonalInfo)
 admin.site.register(ProfessionalExperience)
 admin.site.register(AcademicBackground)
--- backend/candidates/models.py
+++ backend/candidates/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
+class ContactInfo(models.Model):
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+
+    def __str__(self):
+        return f'ID: {self.id} | Email: {self.email} | Phone: {self.phone}'
+
 class PersonalInfo(models.Model):
     name = models.CharField(max_length=255)
     date_of_birth = models.DateField()
     contact_info = models.OneToOneField("ContactInfo", on_delete=models.CASCADE , related_name="personal_info")
 
-class ContactInfo(models.Model):
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    address = models.TextField()
+    def __str__(self):
+        return f'ID: {self.id} | Name: {self.name} | Contact email: {self.contact_info.email}'
 
 class ProfessionalExperience(models.Model):
     position = models.CharField(max_length=255)
@@ -16,11 +22,17 @@ class ProfessionalExperience(models.Model):
     start_date = models.DateField()
     end_date = models.DateField(blank=True, null=True)
     description = models.TextField()
-    personal_info = models.ForeignKey("PersonalInfo", on_delete=models.CASCADE, related_name="professional_experiences")
+    contact_info = models.ForeignKey("ContactInfo", on_delete=models.CASCADE, related_name="professional_experiences")
+
+    def __str__(self):
+        return f'Position: {self.position} | Company: {self.company} | Contact email: {self.contact_info.email}'
 
 class AcademicBackground(models.Model):
     institution = models.CharField(max_length=255)
     course = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField(blank=True, null=True)
-    personal_info = models.ForeignKey("PersonalInfo", on_delete=models.CASCADE, related_name="academic_backgrounds")
+    contact_info = models.ForeignKey("ContactInfo", on_delete=models.CASCADE, related_name="academic_backgrounds")
+
+    def __str__(self):
+        return f'Institution: {self.institution} | Course: {self.course} | Contact email: {self.contact_info.email}'
