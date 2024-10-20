107367eddfb0d1445086e30b521d066abb2be2a8
Thales <thalesondas0@gmail.com>
2024-10-20 18:31:25 -0300

    Creating simple viewsets for CRUD

--- backend/candidates/views.py
+++ backend/candidates/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import PersonalInfo, ContactInfo, ProfessionalExperience, AcademicBackground
+from .serializers import PersonalInfoSerializer, ContactInfoSerializer, ProfessionalExperienceSerializer, AcademicBackgroundSerializer
 
-# Create your views here.
+class PersonalInfoViewSet(viewsets.ModelViewSet):
+    queryset = PersonalInfo.objects.all()
+    serializer_class = PersonalInfoSerializer
+
+class ContactInfoViewSet(viewsets.ModelViewSet):
+    queryset = ContactInfo.objects.all()
+    serializer_class = ContactInfoSerializer
+
+class ProfessionalExperienceViewSet(viewsets.ModelViewSet):
+    queryset = ProfessionalExperience.objects.all()
+    serializer_class = ProfessionalExperienceSerializer
+
+class AcademicBackgroundViewSet(viewsets.ModelViewSet):
+    queryset = AcademicBackground.objects.all()
+    serializer_class = AcademicBackgroundSerializer
