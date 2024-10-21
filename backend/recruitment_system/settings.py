bf5604307ab5d2259404c98ba543be60edb242ec
Thales <thalesondas0@gmail.com>
2024-10-21 15:25:29 -0300

    Adding django-cors-headers

--- backend/recruitment_system/settings.py
+++ backend/recruitment_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third-party apps
     'rest_framework',
+    'corsheaders',
     # Local apps
     'candidates',
 ]
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "recruitment_system.urls"
@@ -125,3 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
