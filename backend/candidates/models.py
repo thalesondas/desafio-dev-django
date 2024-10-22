0fcffbe56136b3c1966f2cf88c0e87ca85df3532
Thales <thalesondas0@gmail.com>
2024-10-22 18:12:03 -0300

    Adding backend validations

--- backend/candidates/models.py
+++ backend/candidates/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import datetime
+import re
 
 class ContactInfo(models.Model):
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
     address = models.TextField()
+
+    def clean(self):
+        if not re.match(r'^\(\d{2}\) \d{5}-\d{4}$', self.phone):
+            raise ValidationError("Número de telefone inválido. O formato deve ser (XX) XXXXX-XXXX")
 
     def __str__(self):
         return f'ID: {self.id} | Email: {self.email} | Phone: {self.phone}'
@@ -12,6 +19,10 @@ class PersonalInfo(models.Model):
     name = models.CharField(max_length=255)
     date_of_birth = models.DateField()
     contact_info = models.OneToOneField("ContactInfo", on_delete=models.CASCADE , related_name="personal_info")
+
+    def clean(self):
+        if self.date_of_birth > datetime.date.today():
+            raise ValidationError("A data de nascimento não pode ser uma data futura.")
 
     def __str__(self):
         return f'ID: {self.id} | Name: {self.name} | Contact email: {self.contact_info.email}'
@@ -24,6 +35,10 @@ class ProfessionalExperience(models.Model):
     description = models.TextField(blank=True, null=True)
     contact_info = models.ForeignKey("ContactInfo", on_delete=models.CASCADE, related_name="professional_experiences")
 
+    def clean(self):
+        if self.exp_end_date and self.exp_end_date < self.exp_start_date:
+            raise ValidationError("A data de término da experiência não pode ser anterior à data de início")
+
     def __str__(self):
         return f'Position: {self.position} | Company: {self.company} | Contact email: {self.contact_info.email}'
 
@@ -33,6 +48,10 @@ class AcademicBackground(models.Model):
     acad_start_date = models.DateField()
     acad_end_date = models.DateField(blank=True, null=True)
     contact_info = models.ForeignKey("ContactInfo", on_delete=models.CASCADE, related_name="academic_backgrounds")
+
+    def clean(self):
+        if self.acad_end_date and self.acad_end_date < self.acad_start_date:
+            raise ValidationError("A data de término do curso não pode ser anterior à data de início")
 
     def __str__(self):
         return f'Institution: {self.institution} | Course: {self.course} | Contact email: {self.contact_info.email}'
--- backend/candidates/serializers.py
+++ backend/candidates/serializers.py
@@ -1,22 +1,74 @@
 from rest_framework import serializers
 from .models import PersonalInfo, ContactInfo, ProfessionalExperience, AcademicBackground
 
+class ContactInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactInfo
+        fields = '__all__'
+
+    def create(self, validated_data):
+        contact_info = ContactInfo(**validated_data)
+        contact_info.full_clean()
+        contact_info.save()
+        return contact_info
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.full_clean()
+        instance.save()
+        return instance
+
 class PersonalInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = PersonalInfo
         fields = '__all__'
 
-class ContactInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactInfo
-        fields = '__all__'
+    def create(self, validated_data):
+        personal_info = PersonalInfo(**validated_data)
+        personal_info.full_clean()
+        personal_info.save()
+        return personal_info
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.full_clean()
+        instance.save()
+        return instance
 
 class ProfessionalExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfessionalExperience
         fields = '__all__'
 
+    def create(self, validated_data):
+        experience = ProfessionalExperience(**validated_data)
+        experience.full_clean()
+        experience.save()
+        return experience
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.full_clean()
+        instance.save()
+        return instance
+
 class AcademicBackgroundSerializer(serializers.ModelSerializer):
     class Meta:
         model = AcademicBackground
         fields = '__all__'
+
+    def create(self, validated_data):
+        academic_background = AcademicBackground(**validated_data)
+        academic_background.full_clean()
+        academic_background.save()
+        return academic_background
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.full_clean()
+        instance.save()
+        return instance
